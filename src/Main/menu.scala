186a6d25af50d7116f20f12b71639f12c03abc0c
sullivan1513 <sullivan1513@gmail.com>
2014-10-04 02:02:07 -0400

    Added basic template

--- src/Main/menu.scala
+++ src/Main/menu.scala
@@ -1,5 +1,36 @@
 package Main
 
-object Menu {
 
+object Menu {
+	import scala.swing._
+	import scala.swing.BorderPanel.Position._
+	import event._
+	import java.awt.{ Color, Graphics2D }
+	import scala.util.Random
+	// Build menu
+  val frame = new MainFrame {
+	 title = "Computer Science 250 Propositional Logic Calculator"
+	   background = Color.black
+	 val label = new Label("CS250 Propositional Logic Calculator")
+	 //contents = new TextComponent()
+	 val button = new Button{
+	   text = "CALCULATE"
+	   foreground = Color.black
+	   foreground = Color.red
+	 }
+	 val textField = new TextField(20){
+	   border = Swing.EmptyBorder(30)
+	 }
+	 // Intro
+	   // Instructions
+	 //Text field submission
+	 // Options
+	 contents = new BorderPanel {
+	   layout(label) = North
+	   layout(button) = South
+	   layout(textField) = Center
+	 }
+	 
+	size = new Dimension(500,500)
+  }
 }
